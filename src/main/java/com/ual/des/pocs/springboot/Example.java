8584b3653595bb7ef0190b6d77b4cd1192e3c982
kiranonly <kiranonly@gmail.com>
2018-09-29 11:48:50 -0500

    Update Example.java

--- src/main/java/com/ual/des/pocs/springboot/Example.java
+++ src/main/java/com/ual/des/pocs/springboot/Example.java
@@ -3,7 +3,7 @@ package com.ual.des.pocs.springboot;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
-@RestController(/test)
+@RestController("/test")
 @SpringBootApplication
 public class Example {
     public static void main(String[] args) {
