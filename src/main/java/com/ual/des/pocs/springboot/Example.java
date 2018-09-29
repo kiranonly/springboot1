6191d77fbc5a877a8ba9ca4c6b202488dc053c4c
kiranonly <kiranonly@gmail.com>
2018-09-29 13:53:56 -0500

    Update Example.java

--- src/main/java/com/ual/des/pocs/springboot/Example.java
+++ src/main/java/com/ual/des/pocs/springboot/Example.java
@@ -4,12 +4,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 @RestController
-@SpringBootApplication
 public class Example {
-    public static void main(String[] args) {
-        SpringApplication.run(Example.class, args);
-    }
-
+   
      @GetMapping("/hi")
     String greet() {
         return "hello!!!!";
