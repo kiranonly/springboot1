4c914489e473aa2fb79b6cdba64e3c895856f4b8
kiranonly <kiranonly@gmail.com>
2018-09-30 15:49:49 -0500

    Update Example.java

--- src/main/java/com/ual/des/pocs/springboot/Example.java
+++ src/main/java/com/ual/des/pocs/springboot/Example.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Example {
    
-     @GetMapping("/")
+     @GetMapping("test")
     String greet() {
         return "hello!!!!";
     }
