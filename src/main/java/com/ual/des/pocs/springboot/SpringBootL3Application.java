1888b86e9a5ddb9129b3028c29de47fe30f16d8c
kiranonly <kiranonly@gmail.com>
2018-09-30 16:00:45 -0500

    Update SpringBootL3Application.java

--- src/main/java/com/ual/des/pocs/springboot/SpringBootL3Application.java
+++ src/main/java/com/ual/des/pocs/springboot/SpringBootL3Application.java
@@ -1,8 +1,11 @@
 package com.ual.des.pocs.springboot;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SpringBootL3Application extends SpringBootServletInitializer {
@@ -11,7 +14,7 @@ public class SpringBootL3Application extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootL3Application.class);
 	}
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootL3Application.class, args);
 	}
